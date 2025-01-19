0462a3b680e4953ab95445d60dad1fe58ace7354
Crawcraw <crawcraw@crau.at.home>
2025-01-19 14:48:43 +0100

    print report feature

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 def count_words(content: str):
     words = content.split()
     return len(words)
@@ -10,11 +12,28 @@ def count_chars(content: str):
     return result
 
 def count_letters_sorted(counts: dict):
-    c_lst = counts.items()
+    c_list = []
+    for c in string.ascii_lowercase:
+        if c in counts:
+            c_list.append((c, counts[c]))
+
+    return sorted(c_list, key= lambda x: x[1], reverse=True)
+
+def print_report(fname: str):
+    with open(fname) as f:
+        content = f.read()
+
+        word_count = count_words(content)
+        letter_count = count_letters_sorted(count_chars(content))
+
+        print(f"--- Begin report of {fname} ---")
+        print(f"{word_count} words found in the document\n")
+
+        for c, cnt in letter_count:
+            print(f"The '{c}' character was found {cnt} times")
+        print("--- End report ---")
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_content = f.read()
-        print(count_chars(file_content))
+    print_report("books/frankenstein.txt")
 
 main()
